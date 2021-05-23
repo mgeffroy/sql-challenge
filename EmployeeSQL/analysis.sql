5496c1e3f9d0f06326ff34e9bb1f92fb4c67dc91
mgeffroy <79372976+mgeffroy@users.noreply.github.com>
2021-05-22 21:33:46 -0500

    Query for counting last names

--- EmployeeSQL/analysis.sql
+++ EmployeeSQL/analysis.sql
@@ -51,4 +51,7 @@ ORDER BY e.last_name;
 
 
 --In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
-
+SELECT last_name, COUNT(last_name) AS "Last Names"
+FROM Employees
+GROUP BY last_name
+ORDER BY last_name DESC; 
