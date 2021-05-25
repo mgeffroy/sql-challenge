fdc1331af312dd4c3571d05cd6bb044298577209
mgeffroy <79372976+mgeffroy@users.noreply.github.com>
2021-05-25 01:17:06 -0500

    Added Last Name column

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -97,7 +97,7 @@ ORDER BY e.emp_no;
 
 
 --In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
-SELECT e.last_name, COUNT(last_name) AS "Last Names"
+SELECT e.last_name AS "Last Name", COUNT(last_name) AS "Last Names"
 FROM Employees AS e
 GROUP BY last_name
 ORDER BY last_name DESC; 
